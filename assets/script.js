d66254689fa5ca22357c0ddecdcec7897108d7c3
Spencer Jagernauth <rita.jagernauth01@gmail.com>
2022-07-30 16:20:16 -0400

    js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
-//Choice objects
-var letters: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ";
-var special characters: "!@#$%^&*()<>?~*";
-var numbers: "0123456789";
-
+///The choice objects 
 var letters
 var symbols
 var numbers
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz"; 
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+var specialSym = "!@#$%^&*()<>?~*";
+var numbers = "0123456789";
+
 
 //Asking how long the password is
 var generatePasswordLength = function () {
@@ -21,6 +23,55 @@ var generatePasswordLength = function () {
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var resetBtn = document.querySelector("#reset");
+
+//Generating password and combining choice objects
+function generatePassword() {
+    var passwordOptions = questions();
+    var possibleCombo = [];
+    var finalPassword = "";
+  
+  
+  
+    if (passwordOptions.askNumbers) {
+      for (var i of numbers)
+        possibleCombo.push(i);
+    }
+    if (passwordOptions.askLowerCase) {
+      for (var i of lowerCase)
+        possibleCombo.push(i);
+    }
+    if (passwordOptions.askUpperCase) {
+      for (var i of upperCase)
+        possibleCombo.push(i);
+    }
+    if (passwordOptions.askSpecial) {
+      for (var i of special)
+        possibleCombo.push(i);
+    }
+  
+  
+    console.log(possibleCombo);
+  
+  
+    for (var i = 0; i < passwordOptions.length; i++) {
+      finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
+      
+    }
+    console.log(finalPassword);
+  
+    return finalPassword;
+  }
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+
+
+
 
 // Write password to the #password input
 function writePassword() {
