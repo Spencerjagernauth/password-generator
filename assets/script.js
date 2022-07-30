2ddced9e5bda9d2193b609e9475e3ddd0dc3337e
Spencer Jagernauth <rita.jagernauth01@gmail.com>
2022-07-30 15:35:14 -0400

    js

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,44 @@
-
-
 //Choice objects
-var lowercase: "abcdefghijklmnopqrstuvwxyz";
-var uppercase: "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+var letters: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ";
 var special characters: "!@#$%^&*()<>?~*";
-var number: "0123456789";
+var numbers: "0123456789";
 
+var letters
+var symbols
+var numbers
 
 //Asking how long the password is
-var passwordLengthCheck = function() {
-    var passwordLength = window.prompt("Chose characters to specify between 8 and 128 for password length.");
+var generatePasswordLength = function () {
+    var passwordLength = window.prompt("Please choose characters between 8 and 128");
     if (passwordLength === "" || passwordLength === null) {
-      window.alert("Fields left empty. Please try again.");
-      passwordLengthCheck()
+        window.alert("Fields left empty. Please try again.");
+        passwordLengthCheck()
     }
-  
+
     passwordLength = parseInt(passwordLength);
     console.log(passwordLength);
-  
-    // mAKE SURE THE PASSWORD IS BETWEEN 8-128 CHARACRTERS
-    if (passwordLength >= 8 && passwordLength <= 128) {
-      passwordInfo.lengthNumber = passwordLength;
-    } else {
-      window.alert("Please enter characters between 8 and 128. Try again!");
-      passwordLengthCheck();
-    }
-  };
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+var resetBtn = document.querySelector("#reset");
+
+// Write password to the #password input
+function writePassword() {
+
+  password.length = document.getElementById("allowed-length").value;
+  password.letters = document.getElementById("allowed-letters").value;
+  password.numbers = document.getElementById("allowed-numbers").value;
+  password.special = document.getElementById("allowed-special").value;
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password.generate();
+
+}
+
+var advancedForm = document.getElementById("advancedForm");
+advancedForm.onsubmit = function(e) { e.preventDefault(); }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+resetBtn.addEventListener("click", resetAdvancedFields);
